d586423ebb452349d7b1b18518b0522b776f5ec7
Alex Rothenberg <alexroth96@gmail.com>
2019-11-05 16:30:27 -0500

    add some design changes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
-import { generateQuestionnaire } from "./javascripts/contentControl.js";
-import { scrollToSection } from "./javascripts/menu";
+import { generateQuestionnaire } from './javascripts/contentControl.js';
+import { scrollToSection } from './javascripts/menu';
 
 const genSurvey = generateQuestionnaire();
 const renderSurvey = genSurvey[0];
-const changeBtns = genSurvey[2];
 
 const loadFile = () => {
-  renderSurvey();
+	renderSurvey();
 
-  changeBtns();
-
-  scrollToSection();
+	scrollToSection();
 };
 
 (() => {
-  if (document.readyState == "loading") {
-    console.log(loadFile());
-    document.addEventListener("DOMContentLoaded", loadFile);
-  } else {
-    console.log(loadFile());
-    loadFile();
-  }
+	if (document.readyState == 'loading') {
+		console.log(loadFile());
+		document.addEventListener('DOMContentLoaded', loadFile);
+	} else {
+		console.log(loadFile());
+		loadFile();
+	}
 })();
